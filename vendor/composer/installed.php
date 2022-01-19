55c8a3f48ff3a268db96a7062afae5595fdb0821
Shane Bishop <shanebishop@ewww.io>
2022-01-18 22:51:17 -0700

    update to pel 0.9.10
    Move dependency to composer and update to 0.9.10.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'f137804260e967fc25490ecfb7d5de0f95330ead',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'f137804260e967fc25490ecfb7d5de0f95330ead',
+            'dev_requirement' => false,
+        ),
+        'lsolesen/pel' => array(
+            'pretty_version' => '0.9.10',
+            'version' => '0.9.10.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../lsolesen/pel',
+            'aliases' => array(),
+            'reference' => '04ecb8a29e4b1628414193b0df9294232a44f8a9',
+            'dev_requirement' => false,
+        ),
+    ),
+);
